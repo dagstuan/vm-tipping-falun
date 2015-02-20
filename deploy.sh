181c103c5edebc259b0afa2d559d5c034a28dd78
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-20 18:45:38 +0100

    Adds fonts.

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,11 @@
 echo saving away your changes..
 git stash
 
-echo pull rebasing master..
-git pull --rebase origin master
-
 echo bundling..
 ./node_modules/.bin/webpack -p
 
 echo committing..
-git add build/main.entry.js
+git add build/*
 git add index.html
 git commit -m "dump dist"
 
@@ -17,17 +14,17 @@ echo moving to gh-pages..
 git checkout gh-pages
 
 echo checking out files from master..
-git checkout master -- build/main.entry.js
+git checkout master -- build
 git checkout master -- index.html
 
-git add build/main.entry.js
+git add build
 git add index.html
 
 echo committing..
 git commit -m "deploy"
 
 echo deploying..
-git push origin gh-pages
+git push -f origin gh-pages
 
 echo moving back..
 git checkout master
