6df6c1c759f7c1b69d24150e5eec1d8c08eb271b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-20 09:15:58 +0100

    comments

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+echo bundling..
 ./node_modules/.bin/webpack -p
 
+echo committing..
 git add build/main.entry.js
 git add index.html
 git commit -m "dump dist"
 
+echo moving to gh-pages
 git checkout gh-pages
 
 git checkout master -- build/main.entry.js
@@ -13,7 +16,11 @@ git checkout master -- index.html
 git add build/main.entry.js
 git add index.html
 
+echo committing..
 git commit -m "deploy"
+
+echo deploying..
 git push origin gh-pages
 
+echo moving back
 git checkout master
