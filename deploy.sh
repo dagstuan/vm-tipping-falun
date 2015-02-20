36ccae4f61e69f5f900ea21a41b085236181de9b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-20 16:45:34 +0100

    Improves deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo saving away your changes..
+git stash
+
+echo pull rebasing master..
+git pull --rebase origin master
+
 echo bundling..
 ./node_modules/.bin/webpack -p
 
@@ -10,6 +16,7 @@ git commit -m "dump dist"
 echo moving to gh-pages
 git checkout gh-pages
 
+echo checking out files from master..
 git checkout master -- build/main.entry.js
 git checkout master -- index.html
 
@@ -24,3 +31,4 @@ git push origin gh-pages
 
 echo moving back
 git checkout master
+git stash pop
