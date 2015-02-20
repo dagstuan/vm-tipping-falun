08033d9a021fbff857df3a30f01754f240a1adc3
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-20 09:13:13 +0100

    Commit to master as well.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ./node_modules/.bin/webpack -p
 
+git add build/main.entry.js
+git add index.html
+git commit -m "dump dist"
+
 git checkout gh-pages
 
 git checkout master -- build/main.entry.js
